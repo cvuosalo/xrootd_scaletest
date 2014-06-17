f8174087a2197f133af6c207940780e478686140
Carl <cvuosalo@cern.ch>
2014-06-17 12:35:48 -0500

    Added description of new target_time parameter for readjobsplot.py.

--- filereadtests/runplots.sh
+++ filereadtests/runplots.sh
@@ -1,60 +1,60 @@
-# python mkreadtestplots.py plaintest.lst 491 20 1392674604 T2_US_Nebraska
-# python mkreadtestplots.py vreadtest.lst 194 10 1392673957 T2_US_Nebraska
-# python mkreadtestplots.py ramptstfeb19.lst 3573 350 1392829192 T2_US_Nebraska
-# python mkreadtestplots.py ramptstfeb19b.lst 5078 250 1392851660 T2_US_Nebraska
-# python mkreadtestplots.py job200feb20.lst 3280 160 1392919580 T2_US_Nebraska
-# python mkreadtestplots.py job600feb20.lst 5709 570 1392938621 T2_US_Nebraska
-# python mkreadtestplots.py job680feb21.lst 4545 225 1393003581 T2_US_Nebraska
-# python mkreadtestplots.py job680hiratefeb21.lst 4551 225 1393012985 T2_US_Nebraska
-# python readjobsplot.py job680hiratefeb21.lst 4551 225 1393012985 T2_US_Nebraska
-# python readjobsplot.py job500_9.6_4feb21.lst 2048 100 1393023049 T2_US_Nebraska
-# python readjobsplot.py ucsd300_2.4_1.lst 1200 60 1393273290 T2_US_UCSD
-# python readjobsplot.py ucsd340_2.4_1.lst 1509 75 1393275167 T2_US_UCSD
-# python readjobsplot.py ucsd150_2.4_1.lst 2287 110 1393277922 T2_US_UCSD
-# python readjobsplot.py nebfeb18_2.4_1.lst 1916 95 1393281714 T2_US_Nebraska
-# python readjobsplot.py ucsd275_2.4_1.lst 1690 80 1393284531 T2_US_UCSD
-# python readjobsplot.py ucsd800_2.4_10.lst 2936 140 1393350993 T2_US_UCSD
-# python readjobsplot.py florida2.4_10.lst 2371 120 1393364689 T2_US_Florida
-# python readjobsplot.py mit2.4_10.lst 2612 130 1393373328 T2_US_MIT
-# python readjobsplot.py mitfeb26_2.4_10.lst 2968 130 1393456690 T2_US_MIT
-# python readjobsplot.py floridafeb27_2.4_10.lst 4333 200 1393517516 T2_US_Florida
-# python readjobsplot.py floridafeb27b_2.4_10.lst 4310 200 1393526800 T2_US_Florida
-# python readjobsplot.py nebfeb27_2.4_10.lst 4311 200 1393534933 T2_US_Nebraska
-# python readjobsplot.py nebfeb28_2.4_10.lst 4490 221 1393617601 T2_US_Nebraska
-# python readjobsplot.py nebfeb28fix_2.5_10.lst 4312 240 1393629813 T2_US_Nebraska
-# python readjobsplot.py nebmar3_2.5_10.lst 4311 240 1393878319 0.034 T2_US_Nebraska
-# python readjobsplot.py mitmar3fix_2.5_10.lst 4298 240 1393886907 T2_US_MIT
-# python readjobsplot.py mitmar4_2.5_10.lst 4287 240 1393962158 T2_US_MIT
-# python readjobsplot.py wiscmar6_2.5_10.lst 4296 210 1394151976 T2_US_Wisconsin
-# python readjobsplot.py ucsdmar7_2.5_10.lst 4271 210 1394208994 T2_US_UCSD
-# python readjobsplot.py wiscwiscredmar7_2.5_10.lst 4297 240 1394225015 T2_US_Wisconsin
-# python readjobsplot.py mitmar7_2.5_10.lst 2236 120 1394234715 T2_US_MIT
-# python readjobsplot.py mitmar10_2.5_10.lst 4292 240 1394482396 0.042 T2_US_MIT
-# python readjobsplot.py wiscmar10_2.5_10.lst 4340 240 1394494206 T2_US_Wisconsin
-# python readjobsplot.py ucsdmar11_2.5_10.lst 8156 400 1394576173 0.079 T2_US_UCSD 
-# python readjobsplot.py floridamar12_2.5_10.lst 4315 200 1394645350 0.049 T2_US_Florida
-# python readjobsplot.py floridamar12b_2.5_10.lst 4316 240 1394664574 0.049 T2_US_Florida
-# python readjobsplot.py nebmar13_2.5_10.lst 4298 240 1394729162 0.034 T2_US_Nebraska
-# python readjobsplot.py floridamar13_2.5_10.lst 4316 240 1394750199 0.049 T2_US_Florida
-# python readjobsplot.py ucsdmar14_2.5_10.lst 4307 240 1394814014 0.079 T2_US_UCSD 
-# python readjobsplot.py mitmar17_2.5_10.lst 4266 250 1395082265 0.042 T2_US_MIT
-# python readjobsplot.py wiscmar17a_2.5_10.lst 15534 720 1395083032 0.021 T2_US_Wisconsin
-# python readjobsplot.py wiscmar17b_2.5_10.lst 15181 720 1395098858 0.021 T2_US_Wisconsin
-# python readjobsplot.py nebmar18_2.5_10.lst 4294 240 1395179755 0.034 T2_US_Nebraska
-# python readjobsplot.py floridamar19_2.5_10.lst 4276 240 1395242570 0.049 T2_US_Florida
-# python readjobsplot.py wiscnbmar19_2.5_10.lst 4285 240 1395244746 0.021 T2_US_Wisconsin
-# python readjobsplot.py floridamar19b_2.5_10.lst 4272 240 1395257497 0.049 T2_US_Florida
-# python readjobsplot.py nebmar20_2.5_10.lst 4307 240 1395354501 0.034 T2_US_Nebraska
-# python readjobsplot.py nebvrmar21_2.5_10.lst 5371 300 1395429414 0.034 T2_US_Nebraska
-# python readjobsplot.py ucsdvrmar21_2.5_10.lst 5384 300 1395443216 0.079 T2_US_UCSD 
-# python readjobsplot.py floridavrmar27_2.5_10.lst 5519 270 1395949591 0.049 T2_US_Florida
-# python readjobsplot.py caltechvrmar29.lst 5365 300 1396118584 0.064 T2_US_Caltech
-# python readjobsplot.py caltechmar31.txt 3500 240 1396286337 0.064 T2_US_Caltech
-# python readjobsplot.py floridamar31.lst 4284 260 1396307483 0.049 T2_US_Florida
-# python readjobsplot.py mitmar17_2.5_10.lst 4266 250 1395082265 0.042 T2_US_MIT
-# python readjobsplot.py ucsdmar14_2.5_10.lst 4307 240 1394814014 0.079 T2_US_UCSD 
-# python readjobsplot.py wiscnbmar19_2.5_10.lst 4285 240 1395244746 0.021 T2_US_Wisconsin
-# python readjobsplot.py floridamar31.lst 4284 260 1396307483 0.049 T2_US_Florida # python readjobsplot.py nebmar18_2.5_10.lst 4294 240 1395179755 0.034 T2_US_Nebraska
-# python readjobsplot.py mitmar17_2.5_10.lst 4266 250 1395082265 0.042 T2_US_MIT
-# python readjobsplot.py ucsdmar14_2.5_10.lst 4307 240 1394814014 0.079 T2_US_UCSD 
-python readjobsplot.py caltechvrmar29.lst 5365 300 1396118584 0.064 T2_US_Caltech
+# python mkreadtestplots.py plaintest.lst 491 20 1392674604 T2_US_Nebraska 4
+# python mkreadtestplots.py vreadtest.lst 194 10 1392673957 T2_US_Nebraska 4
+# python mkreadtestplots.py ramptstfeb19.lst 3573 350 1392829192 T2_US_Nebraska 4
+# python mkreadtestplots.py ramptstfeb19b.lst 5078 250 1392851660 T2_US_Nebraska 4
+# python mkreadtestplots.py job200feb20.lst 3280 160 1392919580 T2_US_Nebraska 4
+# python mkreadtestplots.py job600feb20.lst 5709 570 1392938621 T2_US_Nebraska 4
+# python mkreadtestplots.py job680feb21.lst 4545 225 1393003581 T2_US_Nebraska 4
+# python mkreadtestplots.py job680hiratefeb21.lst 4551 225 1393012985 T2_US_Nebraska 4
+# python readjobsplot.py job680hiratefeb21.lst 4551 225 1393012985 T2_US_Nebraska 4
+# python readjobsplot.py job500_9.6_4feb21.lst 2048 100 1393023049 T2_US_Nebraska 4
+# python readjobsplot.py ucsd300_2.4_1.lst 1200 60 1393273290 T2_US_UCSD 4
+# python readjobsplot.py ucsd340_2.4_1.lst 1509 75 1393275167 T2_US_UCSD 4
+# python readjobsplot.py ucsd150_2.4_1.lst 2287 110 1393277922 T2_US_UCSD 4
+# python readjobsplot.py nebfeb18_2.4_1.lst 1916 95 1393281714 T2_US_Nebraska 4
+# python readjobsplot.py ucsd275_2.4_1.lst 1690 80 1393284531 T2_US_UCSD 4
+# python readjobsplot.py ucsd800_2.4_10.lst 2936 140 1393350993 T2_US_UCSD 4
+# python readjobsplot.py florida2.4_10.lst 2371 120 1393364689 T2_US_Florida 4
+# python readjobsplot.py mit2.4_10.lst 2612 130 1393373328 T2_US_MIT 4
+# python readjobsplot.py mitfeb26_2.4_10.lst 2968 130 1393456690 T2_US_MIT 4
+# python readjobsplot.py floridafeb27_2.4_10.lst 4333 200 1393517516 T2_US_Florida 4
+# python readjobsplot.py floridafeb27b_2.4_10.lst 4310 200 1393526800 T2_US_Florida 4
+# python readjobsplot.py nebfeb27_2.4_10.lst 4311 200 1393534933 T2_US_Nebraska 4
+# python readjobsplot.py nebfeb28_2.4_10.lst 4490 221 1393617601 T2_US_Nebraska 4
+# python readjobsplot.py nebfeb28fix_2.5_10.lst 4312 240 1393629813 T2_US_Nebraska 4
+# python readjobsplot.py nebmar3_2.5_10.lst 4311 240 1393878319 0.034 T2_US_Nebraska 4
+# python readjobsplot.py mitmar3fix_2.5_10.lst 4298 240 1393886907 T2_US_MIT 4
+# python readjobsplot.py mitmar4_2.5_10.lst 4287 240 1393962158 T2_US_MIT 4
+# python readjobsplot.py wiscmar6_2.5_10.lst 4296 210 1394151976 T2_US_Wisconsin 4
+# python readjobsplot.py ucsdmar7_2.5_10.lst 4271 210 1394208994 T2_US_UCSD 4
+# python readjobsplot.py wiscwiscredmar7_2.5_10.lst 4297 240 1394225015 T2_US_Wisconsin 4
+# python readjobsplot.py mitmar7_2.5_10.lst 2236 120 1394234715 T2_US_MIT 4
+# python readjobsplot.py mitmar10_2.5_10.lst 4292 240 1394482396 0.042 T2_US_MIT 4
+# python readjobsplot.py wiscmar10_2.5_10.lst 4340 240 1394494206 T2_US_Wisconsin 4
+# python readjobsplot.py ucsdmar11_2.5_10.lst 8156 400 1394576173 0.079 T2_US_UCSD  4
+# python readjobsplot.py floridamar12_2.5_10.lst 4315 200 1394645350 0.049 T2_US_Florida 4
+# python readjobsplot.py floridamar12b_2.5_10.lst 4316 240 1394664574 0.049 T2_US_Florida 4
+# python readjobsplot.py nebmar13_2.5_10.lst 4298 240 1394729162 0.034 T2_US_Nebraska 4
+# python readjobsplot.py floridamar13_2.5_10.lst 4316 240 1394750199 0.049 T2_US_Florida 4
+# python readjobsplot.py ucsdmar14_2.5_10.lst 4307 240 1394814014 0.079 T2_US_UCSD  4
+# python readjobsplot.py mitmar17_2.5_10.lst 4266 250 1395082265 0.042 T2_US_MIT 4
+# python readjobsplot.py wiscmar17a_2.5_10.lst 15534 720 1395083032 0.021 T2_US_Wisconsin 4
+# python readjobsplot.py wiscmar17b_2.5_10.lst 15181 720 1395098858 0.021 T2_US_Wisconsin 4
+# python readjobsplot.py nebmar18_2.5_10.lst 4294 240 1395179755 0.034 T2_US_Nebraska 4
+# python readjobsplot.py floridamar19_2.5_10.lst 4276 240 1395242570 0.049 T2_US_Florida 4
+# python readjobsplot.py wiscnbmar19_2.5_10.lst 4285 240 1395244746 0.021 T2_US_Wisconsin 4
+# python readjobsplot.py floridamar19b_2.5_10.lst 4272 240 1395257497 0.049 T2_US_Florida 4
+# python readjobsplot.py nebmar20_2.5_10.lst 4307 240 1395354501 0.034 T2_US_Nebraska 4
+# python readjobsplot.py nebvrmar21_2.5_10.lst 5371 300 1395429414 0.034 T2_US_Nebraska 4
+# python readjobsplot.py ucsdvrmar21_2.5_10.lst 5384 300 1395443216 0.079 T2_US_UCSD  4
+# python readjobsplot.py floridavrmar27_2.5_10.lst 5519 270 1395949591 0.049 T2_US_Florida 4
+# python readjobsplot.py caltechvrmar29.lst 5365 300 1396118584 0.064 T2_US_Caltech 4
+# python readjobsplot.py caltechmar31.txt 3500 240 1396286337 0.064 T2_US_Caltech 4
+# python readjobsplot.py floridamar31.lst 4284 260 1396307483 0.049 T2_US_Florida 4
+# python readjobsplot.py mitmar17_2.5_10.lst 4266 250 1395082265 0.042 T2_US_MIT 4
+# python readjobsplot.py ucsdmar14_2.5_10.lst 4307 240 1394814014 0.079 T2_US_UCSD  4
+# python readjobsplot.py wiscnbmar19_2.5_10.lst 4285 240 1395244746 0.021 T2_US_Wisconsin 4
+# python readjobsplot.py floridamar31.lst 4284 260 1396307483 0.049 T2_US_Florida # python readjobsplot.py nebmar18_2.5_10.lst 4294 240 1395179755 0.034 T2_US_Nebraska 4
+# python readjobsplot.py mitmar17_2.5_10.lst 4266 250 1395082265 0.042 T2_US_MIT 4
+# python readjobsplot.py ucsdmar14_2.5_10.lst 4307 240 1394814014 0.079 T2_US_UCSD  4
+python readjobsplot.py caltechvrmar29.lst 5365 300 1396118584 0.064 T2_US_Caltech 4
